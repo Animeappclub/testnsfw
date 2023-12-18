010f5b30efd82cfcba59f16e39584a262e12604a
Pranav Ajay <86167689+unbeatablered@users.noreply.github.com>
2023-12-18 10:56:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from transformers import pipeline
 
-img = Image.open("https://telegra.ph/file/360a776a052a2ae26b28e.jpg")
+img = Image.open("image (1).png")
 classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
 print(classifier(img))
