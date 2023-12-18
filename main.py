467d5ad41b490b65e4c3bb2284f5e81b986e0921
Pranav Ajay <86167689+unbeatablered@users.noreply.github.com>
2023-12-18 13:56:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from PIL import Image
 from transformers import AutoModelForImageClassification, ViTImageProcessor
 
 img = Image.open("image (1).png")
-model = AutoModelForImageClassification.from_pretrained("Falconsai/nsfw_image_detection")
-processor = ViTImageProcessor.from_pretrained('Falconsai/nsfw_image_detection')
+model = AutoModelForImageClassification.from_pretrained("saltacc/anime-ai-detect")
+processor = ViTImageProcessor.from_pretrained('saltacc/anime-ai-detect')
 with torch.no_grad():
     inputs = processor(images=img, return_tensors="pt")
     outputs = model(**inputs)
+    print(output)
     logits = outputs.logits
+    print(logits)
 
 predicted_label = logits.argmax(-1).item()
+print(predicted_label)
 output = model.config.id2label[predicted_label]
 print(output)
